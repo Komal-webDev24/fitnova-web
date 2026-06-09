e4f15f451a79757a8b4369743e56d8a6723bb8d4
Komal-webDev24 <komalharikrishnaprajapati@gmail.com>
2026-06-09 20:46:23 +0530

    Update API URL

--- src/components/ProfileSetup.jsx
+++ src/components/ProfileSetup.jsx
@@ -88,7 +88,7 @@ const ProfileSetup = () => {
     setSuccess(false);
 
     try {
-      const response = await fetch('http://localhost:5000/api/profile-setup', {
+   const response = await fetch('https://fitnova-backend-vv6q.onrender.com/api/profile-setup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
